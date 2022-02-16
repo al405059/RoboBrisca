0505a7c3904837903bd664d321c2669dbdc1b2b8
al405059 <92725828+al405059@users.noreply.github.com>
2022-02-16 13:02:29 +0100

    Update OTLAPlayer.py

--- Players/OTLAPlayer.py
+++ Players/OTLAPlayer.py
@@ -5,45 +5,44 @@ from Game.Heuristic import Heuristic
 from Players.Player import Player
 
 
-
-class OTLAPlayer:
+class OTLAPlayer(Player):
     def __init__(self):
         self.fm = ForwardModel()
         self.ht = Heuristic()
 
 
-def think(self, obs, budget):
-    l = obs.get_list_actions()
-    values = [0, 0, 0]
+    def think(self, obs, budget):
+        l = obs.get_list_actions()
+        values = [0, 0, 0]
 
-    player_id = obs.turn
-    n = obs.playing_cards.len()
-    if n == 0 or n == 2:
-        factor = -1
-    else:
-        factor = 1
+        player_id = obs.turn
+        n = obs.playing_cards.len()
+        if n == 0 or n == 2:
+            factor = -1
+        else:
+            factor = 1
 
-    # while iteraciones
-    # Recoger cartas, barajar y volver a repatir
-    j = 0
-    for action in l:
-        new_obs = obs.clone()
-        value = self.fm.play(new_obs, action, self.ht)
-        n = n + 1
-        other = player_id + 1
-        while n < 4:
-            if other == 4:
-                other = 0
-            # Seleccionar una carta al azar
-            ith = random.choice(range(3))
-            c = new_obs.hands[other].get_card(ith)
-            a = Action(c)
-            # Jugarla
-            value = self.fm.play(new_obs, a, self.ht)
-            n += 1
-            other += 1
+        # while iteraciones
+        # Recoger cartas, barajar y volver a repatir
+        j = 0
+        for action in l:
+            new_obs = obs.clone()
+            value = self.fm.play(new_obs, action, self.ht)
+            n = n + 1
+            other = player_id + 1
+            while n < 4:
+                if other == 4:
+                    other = 0
+                # Seleccionar una carta al azar
+                ith = random.choice(range(3))
+                c = new_obs.hands[other].get_card(ith)
+                a = Action(c)
+                # Jugarla
+                value = self.fm.play(new_obs, a, self.ht)
+                n += 1
+                other += 1
 
-    values[j] += value * factor
-    j += 1
+        values[j] += value * factor
+        j += 1
 
 # buscar el maximo en values
